c7bb317fe2a82da089b740d150b7cc18aa1fe208
sandeshkoiralaherald <np03cs4s210261@heraldcollege.edu.np>
2023-05-19 13:59:56 +0545

    all user modifying

--- alluser.php
+++ alluser.php
@@ -1,3 +1,65 @@
+<?php include "alluser_sidebar.php" ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All User</title>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+    <!--  Charts Plugin -->
+    <script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+    <script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+
+    <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+    <script src="assets/js/demo.js"></script>
+
+    </script>
+    <script src="assets/js/script.js"></script>
+    </script>
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet" />
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet" />
+
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet" />
+    <link rel="stylesheet" href="styles.css">
+    <style>
+        input[type=submit] {
+            background-color: #0ddbaf;
+            border: none;
+            color: white;
+            padding: 14px 30px;
+            text-decoration: none;
+
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+    </style>
+
+</head>
+
 <body>
     <h2>&nbsp;&nbsp;Search By Offence ID</h2>
     <form action="alluser.php" method="post">
@@ -189,3 +251,5 @@
 <div id="payment-status"></div>
 
 </body>
+
+</html>
