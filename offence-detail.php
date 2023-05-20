ba924b31e5716a054f6520db3959bc1c33240adb
sandeshkoiralaherald <np03cs4s210261@heraldcollege.edu.np>
2023-05-20 19:33:14 +0545

    ninth commit

--- offence-detail.php
+++ offence-detail.php
@@ -20,7 +20,7 @@
  							<br>
  							<div class="mapouter">
  								<div class="gmap_canvas"><iframe width="690px" height="294px" id="gmap_canvas" src="https://maps.google.com/maps?q=<?php echo $row['address']; ?>
-							&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.webdesign-muenchen-pb.de"></a></div>
+							&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
  								<style>
  									.mapouter {
  										text-align: right;
